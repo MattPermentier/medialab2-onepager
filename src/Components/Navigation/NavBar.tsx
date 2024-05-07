2fc09518570251764edf97176ccf8e47d41870e2
Matt Permentier <matt.permentier@gmail.com>
2024-05-07 15:02:34 +0200

    Add navbar setup

---
+++ src/Components/Navigation/NavBar.tsx
@@ -0,0 +1,9 @@
+import { AppBar, Typography } from "@mui/material";
+
+export default function NavBar() {
+  return (
+    <AppBar position="static">
+      <Typography>test</Typography>
+    </AppBar>
+  );
+}
